ab5b548dd1cc20eee7a17aa32213e99ccca9f414
= <=>
2023-02-25 16:43:58 +0100

    API

---
+++ app/main.py
@@ -0,0 +1,43 @@
+from io import BytesIO
+from typing import List
+import uvicorn
+from fastapi import FastAPI, File, HTTPException, UploadFile
+from model import load_model, predict, prepare_cli
+#from PIL import Image
+from pydantic import BaseModel
+import json
+import pandas as pd
+
+app = FastAPI()
+model = load_model()
+# Define the response JSON
+
+class Prediction(BaseModel):
+    filename: str
+    content_type: str
+    Pourcentage_de_non_solvabilité: int
+    #Pourcentage_de_solvabilité: int
+@app.post("/predict", response_model=Prediction)
+async def prediction(file: UploadFile = File(...)):
+    # Ensure that the file is an image
+    #if not file.content_type.startswith("image/"):
+    #    raise HTTPException(status_code=400, detail="File provided is not an image.")
+    content = await file.read()
+    cli = json.loads(content)
+        
+    df = pd.read_json(cli)
+    # preprocess the image and prepare it for classification
+    #cli = prepare_cli(content)
+    #chk_id = df['SK_ID_CURR']
+    sample = df 
+    response = predict(sample, model)
+    #response2 = predict2(sample, chk_id, model)
+    # return the response as a JSON
+    return {
+        "filename": file.filename,
+        "content_type": file.content_type,
+        "Pourcentage_de_non_solvabilité": response,
+        #"Pourcentage_de_solvabilité": response2,
+    }
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=5000)
---
+++ app/model.py
@@ -0,0 +1,31 @@
+import numpy as np
+#from PIL import Image
+import lightgbm as lgbm
+import pickle
+import json
+import pandas as pd
+
+def load_model():
+    '''loading the trained model'''
+    pickle_in = open('model_adaboost.pkl', 'rb') 
+    clf = pickle.load(pickle_in)
+    return clf
+   
+    
+def prepare_cli(content):
+    #dicte = json.loads(content)
+    df2 = pd.read_json(content)
+    return df2
+    
+   
+def predict(sample, clf):
+    X=sample
+
+    score = clf.predict_proba(X)[0][1] * 100
+    return score
+    
+#def predict2(sample, id, clf):
+#    X=sample.iloc[:, :-1]
+
+#    score = clf.predict_proba(X)[0][0] * 100
+#    return score
