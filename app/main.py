a6aa38d32a5620b301d78748128c5bff8b92b94c
orkhoven <67419449+orkhoven@users.noreply.github.com>
2023-03-01 13:04:30 +0100

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -12,10 +12,8 @@ app = FastAPI()
 model = load_model()
 # Define the response JSON
 
-class Prediction(BaseModel):
-    Pourcentage_de_non_solvabilité: int
     #Pourcentage_de_solvabilité: int
-@app.post("/predict", response_model=Prediction)
+@app.post("/predict")
 async def prediction(file: UploadFile = File(...)):
     # Ensure that the file is an image
     #if not file.content_type.startswith("image/"):
@@ -31,7 +29,7 @@ async def prediction(file: UploadFile = File(...)):
     response = predict(sample, model)
     #response2 = predict2(sample, chk_id, model)
     # return the response as a JSON
-    return { "Pourcentage_de_non_solvabilité": response }
+    return response 
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=5000)
